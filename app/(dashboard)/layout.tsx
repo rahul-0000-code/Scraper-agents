e6b41c8d64cc2e7d3eb6d57b4da1da2a00308b60
rahul-0000-code <rahulkbhandari272002@gmail.com>
2024-11-14 00:36:21 +0530

    Shadcn integr^n, 2 components

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='flex h-screen'></div>
+  )
+}
+
+export default layout
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,15 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
+	container :{
+		center: true,
+		padding: "2rem",
+		screens: {
+			"2xl": "1400px",
+		},
+	},
   	extend: {
   		backgroundImage: {
   			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
